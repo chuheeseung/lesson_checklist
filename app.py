dfb47e251eabaf9ee5e69ae7488019b1ea65e00b
희승 <62300314+chuheeseung@users.noreply.github.com>
2020-08-27 12:38:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
-
+from bson.objectid import ObjectId
 from flask import Flask, render_template, jsonify, request
-
+from bson import json_util
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
@@ -15,16 +15,16 @@ def home():
 #체크박스 체크하면 저장 CREATE
 @app.route('/check', methods=['POST'])
 def post_checkBox():
-    #클라이언트로부터 데이터를 받기
-    target = request.form['target']
     isChecked = request.form['isChecked']
+    _id = request.form['_id']
+    weekNum = request.form['weekNum']
 
-    list = {
-        'target' : target,
-        'isChecked' : isChecked
-    }
+    if(isChecked == 'true') :
+        checkBool = bool('true')
+    else:
+        checkBool = bool()
 
-    db.checkList.insert_one(list)
+    db.newInputList.update_one({'_id' :  ObjectId(_id)}, {'$set' : {weekNum : checkBool}})
 
     return jsonify({'result':'success','msg':' post 확인!'})
 
@@ -33,9 +33,58 @@ def post_checkBox():
 def get_checkBox():
     result = list(db.checkList.find({},{'_id':False}))
 
-    return jsonify({'result':'success','articles':result})
+    return jsonify({'result':'success','lists':result})
 
-#원래 체크했는데 체크 바뀌는 경우 UPDATE
+
+
+#리스트 새로 추가 CREATE
+@app.route('/input',methods=['POST'])
+def add_input():
+    inputSubject = request.form['inputSubject']
+    categoryList = request.form['categoryList']
+
+    list = {
+        'inputSubject' : inputSubject,
+        'categoryList' : categoryList,
+        'week1' : False,
+        'week2' : False,
+        'week3' : False,
+        'week4' : False,
+        'week5' : False,
+        'week6' : False,
+        'week7' : False,
+        'week8' : False,
+        'week9' : False,
+        'week10' : False,
+        'week11' : False,
+        'week12' : False,
+        'week13' : False,
+        'week14' : False,
+        'week15' : False,
+        'week16' : False
+    }
+
+    db.newInputList.insert_one(list)
+
+    return jsonify({'result' : 'success', 'msg' : 'POST 연결되었습니다!'})
+
+#추가한 리스트 보여주는 GET
+@app.route('/input',methods=['GET'])
+def show_tables() :
+    result = list(db.newInputList.find({}))
+
+    return json_util.dumps({'result':'success','lists':result})
+
+
+
+
+
+
+
+
+
+
+
 
 #체크 다 지워버리기 DELETE
 
